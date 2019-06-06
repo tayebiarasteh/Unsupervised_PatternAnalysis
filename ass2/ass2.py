c51092391e9393cf871858ecf80456db67942a86
smFAU <sacha.medaer@fau.de>
2019-06-06 16:32:59 +0200

    2D features works

--- ass2/ass2.py
+++ ass2/ass2.py
@@ -16,7 +16,7 @@ cdf_raccoon = np.cumsum(pdf_raccoon)
 cdf_raccoon = cdf_raccoon / cdf_raccoon[-1] #normalizing cdf
 
 ####### uniform sampling
-n_samples = 200000            # number of training samples
+n_samples = 100000            # number of training samples
 uni = np.random.uniform(0, 1 - 1e-10, n_samples)
 new_sampled_cdf = np.searchsorted(cdf_raccoon, uni)
 #new_sampled_cdf = pdf_raccoon[new_sampled_cdf]
@@ -28,44 +28,37 @@ background = np.random.uniform(0, pdf_raccoon.shape[0], n0_samples)
 print(background)
 
 ###### running trees
+
+##### target assignement
+Y = np.ones(n_samples)
+Y = np.hstack((Y,np.zeros(n0_samples)))
+print(Y.shape)
+
+##### samples for RandomForest
+X = np.hstack((new_sampled_cdf,background))
+X = X.reshape((-1,1))
+
 xy_features = 1
 
 if (xy_features):
-    ##### target assignement
-    Y = np.ones(n_samples)
-    Y = np.hstack((Y,np.zeros(n0_samples)))
-    print(Y.shape)
-
-    ##### samples for RandomForest
-    X = np.hstack((new_sampled_cdf,background))
-    print(X.reshape((-1,1)).shape)
-
     ########## RandomForest
-    #regr = RandomForestRegressor(max_depth=60, n_estimators=80)
-    regr = ExtraTreesRegressor(max_depth=60, n_estimators=80)
-    regr.fit(X.reshape((-1,1)), Y)
     pixels = [[i] for i in range(pdf_raccoon.shape[0])]
 
 
 else:
-    ##### target assignement
-    Y = np.ones(n_samples)
-    Y = np.hstack((Y,np.zeros(n0_samples)))
-    print(Y.shape)
-
-    ##### samples for RandomForest
-    X = np.hstack((new_sampled_cdf,background))
-    print(X.reshape((-1,1)).shape)
-
+    temp = []
+    col = raccoon.shape[1]
+    for i in range( X.shape[0] ):
+        temp.append([X[i][0]//col, X[i][0]%col])
+    X = temp
     ########## RandomForest
-    #regr = RandomForestRegressor(max_depth=60, n_estimators=80)
-    regr = ExtraTreesRegressor(max_depth=60, n_estimators=80)
-    regr.fit(X.reshape((-1,1)), Y)
-    #pixels = 0
-    #for i in range(raccoon.shape[0])
     pixels = [[i,j] for i in range(raccoon.shape[0]) for j in range(raccoon.shape[1]) ]
 
 
+########## RandomForest
+regr = RandomForestRegressor(max_depth=60, n_estimators=80)
+#regr = ExtraTreesRegressor(max_depth=60, n_estimators=80)
+regr.fit(X, Y)
 print('____________________________________________________')
 #print(pixels)
 output = regr.predict(pixels)
